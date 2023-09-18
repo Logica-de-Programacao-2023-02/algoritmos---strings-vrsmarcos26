de28b36406fe1077659a546b8c4584844a084e07
Marcos Vinícius <98711516+vrsmarcos26@users.noreply.github.com>
2023-09-18 10:12:01 -0300

    Update S20.go

--- Scripts/S20.go
+++ Scripts/S20.go
@@ -1,4 +1,22 @@
-// Solicite ao usuário uma string e informe se ela é está em camelCase e quantas palavras possuí.
-// CamelCase é quando a primeira letra de cada palavra é maiúscula, e as demais são minúsculas.
-// Exemplo: "estaStringEstaEmCamelCase".
+//// Solicite ao usuário uma string e informe se ela é está em camelCase e quantas palavras possuí.
+//// CamelCase é quando a primeira letra de cada palavra é maiúscula, e as demais são minúsculas.
+//// Exemplo: "estaStringEstaEmCamelCase".
 package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var x string
+	fmt.Print("Escreva uma string: ")
+	fmt.Scan(&x)
+
+	count := 1
+	for _, c := range x {
+		if strings.ToUpper(string(c)) == string(c) {
+			count++
+		}
+	}
+}
